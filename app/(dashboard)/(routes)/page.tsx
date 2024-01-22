356b465f5ab0e2946b08b261e6898cc02138bc79
Simon Tamang <gomjasimon555@gmail.com>
2024-01-22 15:19:45 +0545

    layout

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
-const page = () => {
-  return <div>jhkjhkjhlkj</div>;
-};
-
-export default page;
+export default function Home() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import { Sidebar } from "./_components/sidebar";
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-56 flex-col inset-y-0 z-50">
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
       {children}
